2b2de1d84c4a79dcc18ada74f55f48d504325736
Sergio <sergiomatiash8@gmail.com>
2026-04-10 19:39:46 +0300

    Add Docker config (dev/prod) and fix ETL Bronze-to-Silver conversion

--- src/etl/convert_to_parquet.py
+++ src/etl/convert_to_parquet.py
@@ -1,31 +1,26 @@
+import polars as pl
 import os
-import pandas as pd
-from pyspark.sql import SparkSession
 
-spark = SparkSession.builder \
-    .appName("Windows-Fix") \
-    .config("spark.driver.host", "localhost") \
-    .getOrCreate()
 
-try:
-    # 1. Читаємо CSV
-    df = spark.read.csv("data/raw/amazon_reviews.csv", header=True, inferSchema=True)
-    
-    # 2. Конвертуємо в Pandas (це обходить всі проблеми з winutils)
-    # Для домашки об'єм даних дозволяє це зробити без проблем
-    p_df = df.toPandas()
-    
-    # 3. Створюємо папку вручну
-    os.makedirs("data/silver", exist_ok=True)
-    
-    # 4. Пишемо файл
-    p_df.to_parquet("data/silver/reviews.parquet", engine='pyarrow', index=False)
-    
-    print("\n" + "!"*20)
-    print("НАРЕШТІ! Файл записано в data/silver/reviews.parquet")
-    print("!"*20)
+INPUT_CSV = "data/raw/amazon_reviews.csv" 
+OUTPUT_FOLDER = "data/interim/reviews_parquet"
 
-except Exception as e:
-    print(f"\nзнову помилка: {e}")
-finally:
-    spark.stop()
+def convert():
+    if not os.path.exists(INPUT_CSV):
+        print(f"❌ Помилка: Файл {INPUT_CSV} не знайдено!")
+        return
+
+    print("⏳ Читаємо CSV...")
+
+    df = pl.read_csv(INPUT_CSV, ignore_errors=True)
+    
+ 
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    print(f"⏳ Записуємо Parquet у {OUTPUT_FOLDER}...")
+    df.write_parquet(f"{OUTPUT_FOLDER}/reviews.parquet")
+    
+    print("Done")
+
+if __name__ == "__main__":
+    convert()
